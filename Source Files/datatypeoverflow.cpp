bebe6c6dfda750cd06bab4648a6959b83fa021ae
Lisa <llvvss@icloud.com>
2024-03-08 21:27:23 -0700

    ASCII characters and back

--- Source Files/datatypeoverflow.cpp
+++ Source Files/datatypeoverflow.cpp
@@ -8,7 +8,11 @@ int main()
     // cout << intMax + 1;     // result: -2147483648. 
     //                         // It went back around to the min value
 
-    cout << (int)'a' << endl;
-    cout << int('a') << endl;
+    cout << (int)'a' << endl;   // prints the number 97
+    cout << int('a') << endl;   // prints the number 97 (prints 2 circled then 97, for this line and the previous line)
+    cout << int('A') << endl;   // prints the number 65
 
+        // this or this is a "casting operator": (int)'a'   or int('a')
+
+    cout << char(65) << endl;     // prints the letter A
 }
