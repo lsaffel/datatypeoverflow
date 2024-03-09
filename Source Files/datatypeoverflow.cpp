2b6b75f2822ef372fa70f2497334a62144975b58
Lisa <llvvss@icloud.com>
2024-03-08 21:16:27 -0700

    ascii character representation

--- Source Files/datatypeoverflow.cpp
+++ Source Files/datatypeoverflow.cpp
@@ -3,8 +3,12 @@ using namespace std;
 
 int main()
 {
-    int intMax = INT_MAX;
-    cout << intMax << endl; // result: 2147483647
-    cout << intMax + 1;     // result: -2147483648. 
-                            // It went back around to the min value
+    // int intMax = INT_MAX;
+    // cout << intMax << endl; // result: 2147483647
+    // cout << intMax + 1;     // result: -2147483648. 
+    //                         // It went back around to the min value
+
+    cout << (int)'a' << endl;
+    cout << int('a') << endl;
+
 }
